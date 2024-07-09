971d5a2a795c5b4b16aa86bb6a275d7e2c24c5c7
Andriambololomanana Brondone <andriambololomananabrondone@gmail.com>
2024-07-09 14:43:40 +0300

    repository

--- routes/Location.js
+++ routes/Location.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const models = require("../models")
+const { fn, literal } = require('sequelize');
+const axios = require('axios');
 
 router.get("/", async (req, res) => {
   const limit = parseInt(req.query.limit) || 10
@@ -17,6 +19,26 @@ router.get("/", async (req, res) => {
   res.json({ locations: listOfLocation, totalPage: Math.ceil(count / limit) })
 })
 
+router.get("/statistic", async (req, res) => {
+  try {
+    const results = await models.Location.findAll({
+      attributes: [
+        [fn('SUM', literal('nb_jours * taux_journalier')), 'total_loyer'],
+        [fn('MIN', literal('nb_jours * taux_journalier')), 'min_loyer'],
+        [fn('MAX', literal('nb_jours * taux_journalier')), 'max_loyer']
+      ],
+      raw: true // Pour retourner les résultats comme un objet brut
+    });
+
+    const statistics = results[0];
+
+    res.json({ min: statistics.min_loyer, max: statistics.max_loyer, total: statistics.total_loyer });
+  } catch (error) {
+    console.error('Erreur lors du calcul des statistiques:', error);
+    res.status(500).json({ error: 'Erreur lors du calcul des statistiques' });
+  }
+});
+
 router.get("/byId/:id", async (req, res) => {
   const id = req.params.id
   const location = await models.Location.findByPk(id)
@@ -24,17 +46,45 @@ router.get("/byId/:id", async (req, res) => {
 })
 
 router.post("/", async (req, res) => {
-  const post = req.body
-  await models.Location.create(post)
-  res.json(post)
-})
+  const { recaptchaToken, ...post } = req.body;
+
+  // Vérifiez le token reCAPTCHA
+  const secretKey = process.env.RECAPTCHA_SECRETE_KEY;
+  const recaptchaResponse = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
+    params: {
+      secret: secretKey,
+      response: recaptchaToken,
+    },
+  });
+
+  if (!recaptchaResponse.data.success) {
+    return res.json({ error: "error", message: 'Échec de la vérification reCAPTCHA' });
+  } else {
+    await models.Location.create(post);
+    res.json(post);
+  }
+});
 
 router.put("/:id", async (req, res) => {
-  const locationId = req.params.id
-  const post = req.body
-  await models.Location.update(post, { where: { id: locationId } })
-  res.json(post)
-})
+  const locationId = req.params.id;
+  const { recaptchaToken, ...post } = req.body;
+
+  // Vérifiez le token reCAPTCHA
+  const secretKey = process.env.RECAPTCHA_SECRETE_KEY;
+  const recaptchaResponse = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
+    params: {
+      secret: secretKey,
+      response: recaptchaToken,
+    },
+  });
+
+  if (!recaptchaResponse.data.success) {
+    return res.json({ error: true, message: 'Échec de la vérification reCAPTCHA' });
+  } else {
+    await models.Location.update(post, { where: { id: locationId } });
+    res.json(post);
+  }
+});
 
 router.delete("/:id", async (req, res) => {
   const locationId = req.params.id
