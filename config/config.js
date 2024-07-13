4bd3ba8de9976a92c3b3c802ccef637645536020
Andriambololomanana Brondone <andriambololomananabrondone@gmail.com>
2024-07-13 14:36:52 +0300

    repository

--- config/config.js
+++ config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
+    "username": "postgres",
+    "password": "admin",
     "database": "simple-crud",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 };
