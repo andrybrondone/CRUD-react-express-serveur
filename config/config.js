455c213a2f7f9cb24c7d90ea491128755b4c4bac
Andriambololomanana Brondone <andriambololomananabrondone@gmail.com>
2024-07-13 15:11:26 +0300

    repository

--- config/config.js
+++ config/config.js
@@ -9,11 +9,17 @@ module.exports = {
     "dialect": "postgres"
   },
   "test": {
-    "url": process.env.DB_URL,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
-    "url": process.env.DB_URL,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   }
 };
